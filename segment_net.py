a996e918af07ed9834181d55446bc9b6c041e79b
tw-ilson <wilson.th@northeastern.edu>
2022-03-04 17:35:16 -0500

    segnet

--- segment_net.py
+++ segment_net.py
@@ -1,3 +1,4 @@
+import segmentation_models_pytorch as smp
 
 """
 This file will containt the image segmentation network used to pre-process the image obtained from wrist mounted camera. We will take a pre-trained network and tune it to produce a segmentation mask on the object if it is in frame.
