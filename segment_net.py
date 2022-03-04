81f54c1a6415a92fda3ce8aa52c57e4e751499ae
tw-ilson <wilson.th@northeastern.edu>
2022-03-04 17:34:08 -0500

    segnet file

---
+++ segment_net.py
@@ -0,0 +1,4 @@
+
+"""
+This file will containt the image segmentation network used to pre-process the image obtained from wrist mounted camera. We will take a pre-trained network and tune it to produce a segmentation mask on the object if it is in frame.
+"""
