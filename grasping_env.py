b2be4c87e7c8ec42b90c879973217fabf4efaf6e
tw-ilson <wilson.th@northeastern.edu>
2022-03-10 14:48:26 -0500

    mar10

--- grasping_env.py
+++ grasping_env.py
@@ -8,24 +8,35 @@ import pybullet as pb
 import pybullet_data
 import gym
 import time
-from tqdm import tqdm
 import h5py
+import matplotlib.pyplot as plt
 
 import os
 
-import nuro_arm.camera 
-from nuro_arm.robot import robot_arm
+import nuro_arm.robot.robot_arm as robot
+
+WORKSPACE = np.array(((0.10, -0.05, 0.2), # ((min_x, min_y, min_z)
+                      (0.20, 0.05, 0.3))) #  (max_x, max_y, max_z))
+
+READY_JPOS = [0, -1, 1.2, 1.4, 0]
 
 
-
-class HandoverArm(robot_arm.RobotArm):
+class HandoverArm(robot.RobotArm):
     def __init__(self, controller_type='sim', headless=True, realtime=False, workspace=None, pb_client=None, serial_number=None):
         super().__init__(controller_type, headless, realtime, workspace, pb_client, serial_number)
+        
         self.camera_link_index = 12;
         if controller_type == 'sim':
             self._id = self._sim.robot_id;
 
-    
+        self.arm_ready_jpos = READY_JPOS
+
+    def ready(self):
+        '''
+        moves the arm to the 'ready' position (bent, pointing forward toward workspace)
+        '''
+        self.open_gripper()
+        self.move_arm_jpos(self.arm_ready_jpos)
 
 class WristCamera:
     def __init__(self, robot: HandoverArm, img_size: int) -> None:
@@ -51,6 +62,7 @@ class WristCamera:
         '''
         self.img_size = img_size
         self.robot = robot
+        self.type = robot.controller_type
 
         self.computeView()
 
@@ -58,47 +70,56 @@ class WristCamera:
         """
         Computes the view matrix and projection matrix based on the position and orientation of the robot's end effector
         """
-        cam_linkstate = pb.getLinkState(self.robot._id, self.robot.camera_link_index)
-        self.position = [*cam_linkstate[0]]
-        self.orientation = [*pb.getMatrixFromQuaternion(cam_linkstate[1])] + [0, 0, 0]
+        cam_linkstate = pb.invertTransform( \
+                *(pb.getLinkState(self.robot._id, self.robot.camera_link_index)[:2]))
+
+        self.position = cam_linkstate[0]
+        self.orientation = np.reshape(pb.getMatrixFromQuaternion(cam_linkstate[1]), (3, 3), order='F')
+
+        #print(self.orientation)
 
         #pre-allocate view matrix
         self.view_mtx = np.zeros(shape=(4,4))
-        for i in range(4):
+        for i in range(3):
             for j in range(3):
-                self.view_mtx[i, j] = self.orientation[3*i +j]
+                self.view_mtx[i, j] = self.orientation[i, j]
+        for i in range(3):
+            self.view_mtx[3, i] = 0
         for i in range(3):
             self.view_mtx[i, 3] = self.position[i]
         self.view_mtx[3,3] = 1
+        #print(self.view_mtx)
 
-        self.proj_mtx = pb.computeProjectionMatrixFOV(fov=60,
+        self.view_mtx = np.ravel(self.view_mtx, order='F')
+
+        self.proj_mtx = pb.computeProjectionMatrixFOV(fov=90,
                                                       aspect=1,
                                                       nearVal=0.01,
-                                                      farVal=1)
+                                                      farVal=10)
         
-        assert(self.view_mtx.shape == (4, 4))
 
 
-    def get_rgb_image(self) -> np.ndarray:
+    def get_image(self) -> Tuple[np.ndarray, np.ndarray]:
         '''Takes rgb image
         Returns
         -------
         np.ndarray
             shape (H,W,3) with dtype=np.uint8
         '''
-        rgba = pb.getCameraImage(width=self.img_size,
+        rgba, _, mask = tuple(
+                pb.getCameraImage(width=self.img_size,
                                  height=self.img_size,
                                  viewMatrix=self.view_mtx,
                                  projectionMatrix=self.proj_mtx,
-                                 renderer=pb.ER_TINY_RENDERER)[2]
-
-        return rgba[...,:3]
+                                 renderer=pb.ER_TINY_RENDERER)[2:5])
 
 
-class TopDownGraspingEnv(gym.Env):
+        return rgba[...,:3], mask
+
+class HandoverGraspingEnv(gym.Env):
     def __init__(self,
                  episode_length: int=3,
-                 img_size: int=42,
+                 img_size: int=128,
                  render: bool=False,
                 ) -> None:
         '''Pybullet simulator with robot that performs top down grasps of a
@@ -106,9 +127,9 @@ class TopDownGraspingEnv(gym.Env):
         from above.
         '''
         # add robot
-        self.robot = HandoverArm(headless=False)
+        self.robot = HandoverArm(headless=not render)
 
-        self.camera = WristCamera(self.robot, 42);
+        self.camera = WristCamera(self.robot, img_size);
 
         # add object
         self.object_id = pb.loadURDF("assets/urdf/object.urdf")
@@ -117,10 +138,11 @@ class TopDownGraspingEnv(gym.Env):
                           spinningFriction=0.005,
                           rollingFriction=0.005)
         self.object_width = 0.02
+        obj_orn = pb.getBasePositionAndOrientation(self.object_id)[1]
 
-        self.workspace = np.array(((0.10, -0.05), # ((min_x, min_y)
-                                   (0.20, 0.05))) #  (max_x, max_y))
+        pb.resetBasePositionAndOrientation(self.object_id, (0.15, 0, 0), obj_orn)
 
+        self.workspace = WORKSPACE
 
         self.t_step = 0
         self.episode_length = episode_length
@@ -167,39 +189,6 @@ class TopDownGraspingEnv(gym.Env):
         pxy = self.img_size * xy_norm
         return pxy.astype(int)
 
-    def perform_grasp(self, x: float, y: float, theta: float=0) -> bool:
-        '''Perform top down grasp in the workspace.  All grasps will occur
-        at a height of the center of mass of the object (i.e. object_width/2)
-        Parameters
-        ----------
-        x
-            x position of the grasp in world frame
-        y
-            y position of the grasp in world frame
-        theta, default=0
-            target rotation about z-axis of gripper during grasp
-        Returns
-        -------
-        bool
-            True if object was successfully grasped, False otherwise. It is up
-            to you to decide how to determine success
-        '''
-        self.robot.teleport_arm(self.robot.home_arm_jpos)
-        self.robot.set_gripper_state(self.robot.GRIPPER_OPENED)
-
-        pos = np.array((x, y, self.object_width/2))
-
-        self.robot.move_gripper_to(pos, theta, teleport=True)
-        self.robot.set_gripper_state(self.robot.GRIPPER_CLOSED)
-
-        self.robot.move_arm_to_jpos(self.robot.home_arm_jpos)
-
-        # check if object is above plane
-        min_object_height = 0.2
-        obj_height = pb.getBasePositionAndOrientation(self.object_id)[0][2]
-        success = obj_height > min_object_height
-
-        return success
 
     def reset_object_position(self) -> None:
         '''Places object randomly in workspace.  The x,y position should be
@@ -207,13 +196,14 @@ class TopDownGraspingEnv(gym.Env):
         The height of the object should be set such that it sits on the plane
         '''
         ws_padding = 0.01
-        x,y = np.random.uniform(self.workspace[0]+ws_padding,
+        x,y,z = np.random.uniform(self.workspace[0]+ws_padding,
                                 self.workspace[1]-ws_padding)
+                                
         theta = np.random.uniform(-np.pi/2, np.pi/2)
 
-        pos = np.array((x,y,self.object_width/2))
+        pos = np.array((x,y,z))
         quat = pb.getQuaternionFromEuler((np.random.randint(2)*np.pi,0, theta))
-        pb.resetBasePositionAndOrientation(self.object_id, pos, quat)
+        pb.resetBasePositionAndOrientation(self.object_id, pos, quat)  
 
     def reset_object_texture(self) -> None:
         '''Randomly assigns a texture to the object.  Available textures are
@@ -225,35 +215,33 @@ class TopDownGraspingEnv(gym.Env):
                              textureUniqueId=tex_id,
                              rgbaColor=(r,g,b,1))
 
-    def get_obs(self) -> np.ndarray:
-        '''Takes picture using camera
+    def get_obs(self) -> Tuple[np.ndarray, np.ndarray]:
+        '''Takes picture using camera, returns rgb and segmentation mask of image
         Returns
         -------
         np.ndarray
             rgb image of shape (H,W,3) and dtype of np.uint8
         '''
-        return self.camera.get_rgb_image()
+        self.camera.computeView()
+        rgb, mask = self.camera.get_image()
 
+        # remove robot parts from correct segmentation labelling
+        map_fn = lambda x: 0 if x == 1 else x
+        mask = np.vectorize(map_fn)(mask)
 
-
-def watch_policy(env: TopDownGraspingEnv, policy: Optional[Callable]=None):
-    if policy is None:
-        policy = lambda s: env.action_space.sample()
-
-    s = env.reset()
-    while 1:
-        a = policy(s)
-        sp, r, d, info = env.step(a)
-        time.sleep(0.5)
-
-        s = sp.copy()
-        if d:
-            s = env.reset()
-            time.sleep(1)
+        return rgb, mask
+    
+    def plot_obs(self):
+        plt.imshow(self.get_obs()[0])
+        plt.show()
 
 
 if __name__ == "__main__":
-    env = TopDownGraspingEnv(render=True)
+    env = HandoverGraspingEnv(render=False, img_size=200)
+    env.robot.ready()
+    env.plot_obs()
+
     while True:
         [pb.stepSimulation() for _ in range(10)]
         time.sleep(.01)
+
--- segment_net.py
+++ segment_net.py
@@ -4,14 +4,11 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision.transforms import ToTensor
 import segmentation_models_pytorch as smp
 import albumentations as aug
-import h5py
 import cv2
-
-import pybullet as pb
+import h5py
 
 from typing import Dict, Tuple, List
 
-import os
 
 """
 Image segmentation network used to pre-process the image obtained from wrist mounted camera. We will take a pre-trained network and tune it to produce a segmentation mask on the object if it is in frame.
@@ -48,8 +45,10 @@ class ObjectMaskNetwork(torch.Module) :
         image_mask = self.model(x)
         return image_mask
 
-    def train(self, x):
-        pass
+    def compute_loss(self,
+            y_pred: torch.Tensor,
+            y_target: torch.Tensor):
+        return self.loss_fn(y_pred, y_target)
 
 
 class ObjectMaskDataset(Dataset):
@@ -67,17 +66,17 @@ class ObjectMaskDataset(Dataset):
 
     def __init__(self, 
             path_to_hdf5,
-            augmentation: aug.Compose=None, 
-            preprocessing: aug.Compose=None):
+            augmentations: Tuple[str]=None):
         
         with h5py.File(path_to_hdf5, 'r') as hf:
-            self.imgs = np.array(hf['imgs'])
+            self.imgs = np.array(hf['images'])
             self.masks = np.array(hf['masks'])
 
-        self.augmentation = augmentation
-        self.preprocessing = preprocessing
+        if augmentations:
+            self.transform = self.makeTranform(augmentations)
+        #self.preprocessing = preprocessing
 
-    def __getitem__(self, i) -> Dict[str, torch.tensor]:
+    def __getitem__(self, i) -> Dict[str, torch.Tensor]:
         """
         gets a singular sample from the data
         """
@@ -85,31 +84,57 @@ class ObjectMaskDataset(Dataset):
 
         mask = torch.tensor(self.masks[i], dtype=torch.int8)
 
-        sample = {'img': image, 'mask': mask}
 
         if self.augmentation:
-            sample = self.augment(sample)
+            sample = self.transform(image=image, mask=mask)
+        else:
+            sample = {'image': image, 'mask': mask}
 
         return sample
 
     def __len__(self):
         return len(self.imgs)
 
-    def augment(self, images: Dict[str, torch.tensor]) -> Dict[str, torch.tensor]:
+    def makeTransform(self, augmentations: Tuple[str]):
         """
-        applies augmentations to a batch of image-mask mappings
-
-        parameters
-        ===========
-        images:
-        2-element dictionary containing training samples
-            dict { 
-                'img': image tensor, 
-                'mask': mask tensor
-                }
-        returns
-        ==========
-        2-element dictionary containing augmented training samples
+        Initializes the augmentation pipeline with the list of provided augments
         """
 
-        raise NotImplemented
+        pipeline = []
+
+        if 'blur' in augmentations:
+            pipeline.append(
+                    aug.OneOf(
+                        [ 
+                            aug.IAASharpen(p=1),
+                            aug.Blur(blur_limit=3, p=1),
+                            aug.MedianBlur(blur_limit=3, p=1),
+                            aug.MotionBlur(blur_limit=3, p=1),
+                        ],
+                        p=0.9,
+                    )  
+            )
+
+        if 'brightness' in augmentations:
+            pipeline.append(
+                    aug.OneOf(
+                        [ 
+                            aug.CLAHE(p=1),
+                            aug.RandomBrightness(p=1),
+                            aug.RandomGamma(p=1),
+                        ],
+                        p=0.9,
+                    )
+            )
+
+        if 'contrast' in augmentations:
+            pipeline.append(
+                    aug.OneOf(
+                        [ 
+                            aug.RandomContrast(p=1),
+                            aug.HueSaturationValue(p=1),
+                        ]
+                    )
+            )
+
+        return aug.Compose(pipeline)
